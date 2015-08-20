57caba7b15fec1599f21d3ce0ce1f6556716390d
Lennart Bader <bader.lennart@googlemail.com>
2015-08-20 21:18:03 +0200

    Added MyTFG API Support

---
+++ app/src/main/java/de/mytfg/app/android/api/ApiParams.java
@@ -0,0 +1,68 @@
+package de.mytfg.app.android.api;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This class allows to specify parameters to pass to the API.
+ * This is just a wrapper class for Map.
+ */
+public class ApiParams {
+    private Map<String, String> parameters;
+
+    /**
+     * Creates a new empty Set of API-parameters
+     */
+    public ApiParams() {
+        parameters = new HashMap<>();
+    }
+    /**
+     * Creates a new Set of API-parameters.
+     * @param params The params to create as String-Array. Always pass pairs of "key" and "value".
+     */
+    public ApiParams(String... params) {
+        this();
+        if (params != null && params.length > 0 && (params.length % 2 == 0)) {
+            // There are given params and params can be interpreted as pairs of key and value
+            int i = 0;
+            while (i < params.length) {
+                parameters.put(params[i], params[i+1]);
+                i += 2;
+            }
+        }
+    }
+
+    /**
+     * Creates a new Set of API-parameters from a given Map of key / value pairs.
+     * @param params The params to create as String-String Map.
+     */
+    public ApiParams(Map<String, String> params) {
+        this.parameters = params;
+    }
+
+    /**
+     * Adds a new parameter.
+     * @param key The key for the new parameter.
+     * @param value The value to assign.
+     */
+    public void addParam(String key, String value) {
+        parameters.put(key, value);
+    }
+
+    /**
+     * Returns the value for a given key.
+     * @param key The key to search the value for.
+     * @return The value for the given key or Empty String iff key not found.
+     */
+    public String get(String key) {
+        if (parameters.containsKey(key)) {
+            return parameters.get(key);
+        } else {
+            return "";
+        }
+    }
+
+    protected Map<String, String> getMap() {
+        return parameters;
+    }
+}
