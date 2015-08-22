9206fc2672004a5c252d324f5277f56a92d80751
Lennart Bader <bader.lennart@googlemail.com>
2015-08-22 21:08:02 +0200

    Improved Navigation, Added Terminal Input Field, Added FloatingAction Button to TerminalFragment

--- app/src/main/java/de/mytfg/app/android/slidemenu/AbstractFragment.java
+++ app/src/main/java/de/mytfg/app/android/slidemenu/AbstractFragment.java
@@ -3,12 +3,18 @@ package de.mytfg.app.android.slidemenu;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
+import de.mytfg.app.android.slidemenu.items.Navigation;
+import de.mytfg.app.android.slidemenu.items.NavigationItem;
+
 /**
  * Abstract class for used Fragments.
  * Makes Fragments have a title.
  */
 public abstract class AbstractFragment extends Fragment {
     public Bundle args = new Bundle();
+
+    public NavigationItem item;
+
 
     public String getTitle() {
         if (args == null) {
