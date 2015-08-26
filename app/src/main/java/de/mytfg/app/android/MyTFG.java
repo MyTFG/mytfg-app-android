c5444a6d03250fa5162d4941d4028f2fcd2cc3c4
Raphael Lehmann <raphael@rleh.de>
2015-08-26 18:20:58 +0200

    ACRA: add documentation

--- app/src/main/java/de/mytfg/app/android/MyTFG.java
+++ app/src/main/java/de/mytfg/app/android/MyTFG.java
@@ -7,13 +7,21 @@ import org.acra.*;
 import org.acra.annotation.*;
 import org.acra.sender.HttpSender;
 
+/**
+ * ACRA collects crash information (stacktrace, device info, ...) and sends it JSON formated to
+ * acralyzer.
+ * A toast notification is shown to the user.
+ */
 @ReportsCrashes(
+        // upload crash reports to acralyzer https://acra.mytfg.de/acralyzer/_design/acralyzer/index.html (HTTP PUT request)
         httpMethod = HttpSender.Method.PUT,
         reportType = HttpSender.Type.JSON,
         formUri = "https://acra.mytfg.de/acra-mytfg_app_android/_design/acra-storage/_update/report",
+        // HTTP basic auth
         formUriBasicAuthLogin = "mytfg_android",
         formUriBasicAuthPassword = "xP3RvRi+elw9rAXWqMKsGUxOcD4=",
 
+        // show toast when app crashed and report is sent
         mode = ReportingInteractionMode.TOAST,
         resToastText = R.string.crash_toast
 )
