e4f6a8c811c0ee88d5eb820053d5e9946b75eb9f
Lennart Bader <bader.lennart@googlemail.com>
2015-10-03 12:47:15 +0200

    Added getDateString method

--- app/src/main/java/de/mytfg/app/android/utils/TimeUtils.java
+++ app/src/main/java/de/mytfg/app/android/utils/TimeUtils.java
@@ -21,6 +21,18 @@ public class TimeUtils {
         return datetime;
     }
 
+    public static String getDateString(long timestamp) {
+        String datetime;
+        Calendar calendar = Calendar.getInstance(Locale.ENGLISH);
+        calendar.setTimeInMillis(timestamp * 1000);
+        if (calendar.get(Calendar.YEAR) == Calendar.getInstance().get(Calendar.YEAR)) {
+            datetime = DateFormat.format("dd.MM.", calendar).toString();
+        } else {
+            datetime = DateFormat.format("dd.MM.yyyy", calendar).toString();
+        }
+        return datetime;
+    }
+
 
     public static String getDateStringComplete(long timestamp) {
         Calendar calendar = Calendar.getInstance(Locale.ENGLISH);
