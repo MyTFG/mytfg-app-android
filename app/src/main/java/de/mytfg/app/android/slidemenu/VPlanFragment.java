7da3eb6e2b412c2a1ec4865d04e97629e852b833
Lennart Bader <bader.lennart@googlemail.com>
2015-10-19 21:01:25 +0200

    Added new VPlan Layout.  Made VplanFragment Use VPlan Module and new Layout

--- app/src/main/java/de/mytfg/app/android/slidemenu/VPlanFragment.java
+++ app/src/main/java/de/mytfg/app/android/slidemenu/VPlanFragment.java
@@ -2,36 +2,51 @@ package de.mytfg.app.android.slidemenu;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.PagerTabStripV22;
+import android.support.v4.view.ViewPager;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import de.mytfg.app.android.MyTFG;
 import de.mytfg.app.android.R;
-import de.mytfg.app.android.api.ApiParams;
-import de.mytfg.app.android.api.MytfgApi;
+import de.mytfg.app.android.modulemanager.Modules;
+import de.mytfg.app.android.modules.vplan.Vplan;
+import de.mytfg.app.android.modules.vplan.objects.VplanEntry;
+import de.mytfg.app.android.modules.vplan.objects.VplanInfo;
+import de.mytfg.app.android.modules.vplan.objects.VplanObject;
 import de.mytfg.app.android.slidemenu.items.Navigation;
 
 public class VPlanFragment extends AbstractFragment {
     View vplanview;
-    private RecyclerView vPlanList;
-    private String selected_day;
-    JSONArray info_array;
-    boolean pupil;
+    private RecyclerView vPlanList_today;
+    private RecyclerView vPlanList_tomorrow;
+
+    private static final int NUM_PAGES = 2;
+    private ViewPager mPager;
+    private PagerAdapter mPagerAdapter;
+    private PagerTabStripV22 tabs;
+
+    private static String todayTitleDefault = "Heute";
+    private static String tomorrowTitleDefault = "Morgen";
+
+    private String todayTitle = todayTitleDefault;
+    private String tomorrowTitle = tomorrowTitleDefault;
+
+    private boolean multiClass = false;
 
     @Nullable
     @Override
@@ -43,145 +58,117 @@ public class VPlanFragment extends AbstractFragment {
             return null;
         }
 
+        mPager = (ViewPager) vplanview.findViewById(R.id.vplan_pager);
+        mPagerAdapter = new VplanPageAdapter();
+        mPager.setAdapter(mPagerAdapter);
+
+        tabs = (PagerTabStripV22) vplanview.findViewById(R.id.vplan_pager_header);
+        tabs.setTabIndicatorColor(MyTFG.color(R.color.orange_accent));
+
+        LinearLayoutManager linearLayoutManager1 = new LinearLayoutManager(vplanview.getContext());
+        LinearLayoutManager linearLayoutManager2 = new LinearLayoutManager(vplanview.getContext());
+
         // terminalList displays terminalEntries
-        vPlanList = (RecyclerView) vplanview.findViewById(R.id.vPlanList);
-        vPlanList.setHasFixedSize(true);
+        vPlanList_today = (RecyclerView) vplanview.findViewById(R.id.vPlanList_today);
+        // vPlanList_today.setScrollIndicators(View.SCROLL_INDICATOR_RIGHT);
+        vPlanList_today.setHasFixedSize(true);
+        vPlanList_today.setLayoutManager(linearLayoutManager1);
 
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(vplanview.getContext());
-        vPlanList.setLayoutManager(linearLayoutManager);
-
-        Button todayButton = (Button) vplanview.findViewById(R.id.button_today);
-        todayButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                selected_day = "today";
-                refreshVPlanEntries();
-            }
-        });
-
-        Button tomorrowButton = (Button) vplanview.findViewById(R.id.button_tomorrow);
-        tomorrowButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                selected_day = "tomorrow";
-                refreshVPlanEntries();
-            }
-        });
-
-        // set default value for selected_day
-        selected_day = "today";
+        vPlanList_tomorrow = (RecyclerView) vplanview.findViewById(R.id.vPlanList_tomorrow);
+        // vPlanList_tomorrow.setScrollIndicators(View.SCROLL_INDICATOR_RIGHT);
+        vPlanList_tomorrow.setHasFixedSize(true);
+        vPlanList_tomorrow.setLayoutManager(linearLayoutManager2);
 
         refreshVPlanEntries();
         return vplanview;
     }
 
-    private void refreshVPlanEntries() {
-        ApiParams params = new ApiParams();
-        params.addParam("day", selected_day);
-        MytfgApi.ApiCallback callback = new MytfgApi.ApiCallback() {
-            @Override
-            public void callback(boolean success, JSONObject result, int responseCode, String resultStr) {
-                Toast toast;
-                if (success) {
-                    try {
-                        // check, if user is a pupil
-                        pupil = (result.getInt("mode") == 1);
-                        displayInfos(result.getJSONArray("marquee"));
-                        displayHeaderEntries(result.getString("class_str"),
-                                result.getString("status_message"));
-                        // check, if entries are existing
-                        if (result.get("entries") instanceof JSONArray) {
-                            displayVPlanEntries(result.getJSONArray("entries"));
-                        } else if (!result.getBoolean("entries")) {
-                            clearVPlan();
-                        }
-                        return;
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                        toast = Toast.makeText(vplanview.getContext(), "Error parsing JSON",
-                                Toast.LENGTH_LONG);
-                    }
-                } else {
-                    String error = "";
-                    if (resultStr != null) {
-                        error = resultStr;
-                    }
-                    toast = Toast.makeText(vplanview.getContext(), "Fehlgeschlagen: " + responseCode
-                            + " (" + error + ")", Toast.LENGTH_LONG);
-                }
-                toast.show();
+    /**
+     * A Pager to slide the VPlan pages.
+     */
+    public class VplanPageAdapter extends PagerAdapter {
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup collection, int position) {
+            View v = null;
+            if (position == 0) {
+                v = ((MainActivity)MainActivity.context).findViewById(R.id.linearLayout_today);
+            } else {
+                v = ((MainActivity)MainActivity.context).findViewById(R.id.linearLayout_tomorrow);
             }
-        };
-        MytfgApi.call("ajax_vplan_get", params, callback);
-    }
-
-    // save info_array to display information
-    private void displayInfos(JSONArray infos) {
-        info_array = infos;
-    }
-
-    private void displayHeaderEntries(String school_class, String date) {
-        // split string to get useful data
-        String date_new[] = date.split("für");
-        TextView textview_school_class = (TextView) vplanview.findViewById(R.id.school_class);
-        textview_school_class.setText(school_class);
-        TextView textview_date = (TextView) vplanview.findViewById(R.id.date);
-        textview_date.setText(date_new[1]);
-    }
-
-    private void displayVPlanEntries(JSONArray jsonVPlanEntries) throws JSONException {
-        // create list of entries to display in card view
-        List<VPlanEntry> vPlanEntries = new ArrayList<>();
-        for (int i = 0; i < jsonVPlanEntries.length(); i++) {
-            JSONObject obj = jsonVPlanEntries.getJSONObject(i);
-            vPlanEntries.add(new VPlanEntry(
-                            obj.getString("class"),
-                            obj.getString("lesson"),
-                            obj.getString("plan"),
-                            obj.getString("substitution"),
-                            obj.getString("comment"))
-            );
+            return v;
         }
-        RVAdapter adapter = new RVAdapter(vPlanEntries);
-        vPlanList.setAdapter(adapter);
-    }
 
-    private void clearVPlan() {
-        // create list of empty entries to clear plan
-        List<VPlanEntry> vPlanEntries = new ArrayList<>();
-        vPlanEntries.add(new VPlanEntry("", "", "", "", ""));
-        RVAdapter adapter = new RVAdapter(vPlanEntries);
-        vPlanList.setAdapter(adapter);
-    }
+        @Override
+        public CharSequence getPageTitle(int position) {
+            if (position == 0) {
+                return todayTitle;
+            } else {
+                return tomorrowTitle;
+            }
+        }
 
-    class VPlanEntry {
-        String school_class;
-        String lesson;
-        String plan;
-        String substitution;
-        String comment;
-
-        VPlanEntry(String school_class, String lesson, String plan, String comment, String substitution) {
-            this.school_class = school_class;
-            this.lesson = lesson;
-            this.plan = plan;
-            this.substitution = substitution;
-            this.comment = comment;
+        @Override
+        public boolean isViewFromObject(View arg0, Object arg1) {
+            return arg0 == ((View) arg1);
         }
     }
+
+    private void refreshVPlanEntries() {
+        todayTitle = todayTitleDefault;
+        tomorrowTitle = tomorrowTitleDefault;
+
+        Vplan plan = (Vplan) MyTFG.moduleManager.getModule(Modules.VPLAN);
+        plan.getPlan(true, new Vplan.GetPlanCallback() {
+            @Override
+            public void callback(String day, VplanInfo info, List<VplanEntry> entries) {
+                List<VplanObject> objects = new LinkedList<>();
+                objects.add(info);
+                objects.addAll(entries);
+                RVAdapter adapter = new RVAdapter(objects);
+
+                todayTitle = day;
+                mPagerAdapter.notifyDataSetChanged();
+
+                RecyclerView today = (RecyclerView) vplanview.findViewById(R.id.vPlanList_today);
+                today.setAdapter(adapter);
+            }
+        });
+
+        plan.getPlan(false, new Vplan.GetPlanCallback() {
+            @Override
+            public void callback(String day, VplanInfo info, List<VplanEntry> entries) {
+                List<VplanObject> objects = new LinkedList<>();
+                objects.add(info);
+                objects.addAll(entries);
+                RVAdapter adapter = new RVAdapter(objects);
+
+                tomorrowTitle = day;
+                mPagerAdapter.notifyDataSetChanged();
+
+                RecyclerView tomorrow = (RecyclerView) vplanview.findViewById(R.id.vPlanList_tomorrow);
+                tomorrow.setAdapter(adapter);
+            }
+        });
+    }
+
 
     public class RVAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-        List<VPlanEntry> vplanEntries;
+        List<VplanObject> vplanEntries;
 
-        RVAdapter(List<VPlanEntry> entries) {
+        RVAdapter(List<VplanObject> entries) {
             this.vplanEntries = entries;
         }
 
         // get position of item, 0 is vplan_info_view, > 0 are vplan_views
         @Override
         public int getItemViewType(int position) {
-            return position;
+            return vplanEntries.get(position) instanceof VplanEntry ? 1 : 0;
         }
 
 
@@ -191,9 +178,9 @@ public class VPlanFragment extends AbstractFragment {
         }
 
         @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int type) {
             // if this is the vplan_info_view
-            if (i == 0) {
+            if (type == 0) {
                 View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.vplan_info_view_layout, viewGroup, false);
                 return new VPlanInfoViewHolder(v);
             } else {
@@ -204,32 +191,29 @@ public class VPlanFragment extends AbstractFragment {
 
         @Override
         public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int i) {
+            VplanObject obj = vplanEntries.get(i);
             // if this is the vplan_info_view
-            if (i == 0) {
-                // cast to VPlanInfoViewHolder
+            if (obj instanceof VplanInfo) {
                 VPlanInfoViewHolder vPlanInfoViewHolder = (VPlanInfoViewHolder) viewHolder;
                 // get entries and construct String
                 String string_info = "Keine Informationen gespeichert";
-                for (int j = 0; j < info_array.length(); j++) {
-                    try {
-                        string_info = info_array.getString(j) + System.getProperty("line.separator");
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
+                for (int j = 0; j < ((VplanInfo)obj).getInfos().size(); j++) {
+                    string_info = ((VplanInfo)obj).getInfos().get(j) + System.getProperty("line.separator");
                 }
                 vPlanInfoViewHolder.info.setText(string_info);
             } else {
                 // cast to VPlanViewHolder
+                VplanEntry entry = (VplanEntry) obj;
                 VPlanViewHolder vPlanViewHolder = (VPlanViewHolder) viewHolder;
                 // get elements from vplanEntries class
-                String lesson = vplanEntries.get(i).lesson;
-                String school_class = vplanEntries.get(i).school_class;
-                String regular = vplanEntries.get(i).plan;
-                String comment = vplanEntries.get(i).comment;
-                String substitution = vplanEntries.get(i).substitution;
+                String lesson = entry.getLessons();
+                String school_class = entry.getGrade();
+                String regular = entry.getPlan();
+                String comment = entry.getComment();
+                String substitution = entry.getSubstitution();
                 // only display school class in entry cards, if user is no pupil (and has more than
                 // only his own class's entries)
-                if (!pupil) {
+                if (!multiClass) {
                     vPlanViewHolder.schoolClassText.setText(school_class);
                 }
                 // display entries
@@ -237,8 +221,6 @@ public class VPlanFragment extends AbstractFragment {
                 vPlanViewHolder.regularText.setText(regular);
                 vPlanViewHolder.substitutionText.setText(substitution);
                 vPlanViewHolder.substitutionText.setText(comment);
-
-
             }
         }
 
